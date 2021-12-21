ae0c3ae3a80923ac21d9a954cb795e9d03826316
Julia <76563365+JuliaCRabenhorst@users.noreply.github.com>
2021-12-21 16:00:32 +0100

    Add files via upload

--- baby_steps.py
+++ baby_steps.py
@@ -2,7 +2,7 @@ import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 from collections import namedtuple
-from colorama import Fore
+from termcolor import colored
 
 # Create a Point to represent the points.
 Point = namedtuple("Point", "x y")
@@ -11,7 +11,6 @@ result = Point
 
 '''
 using y^2 = x^3 + ax + b
-i only make the positive points
 primes: 
 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 
 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 
@@ -28,19 +27,12 @@ prime = 19      #
 squares = []
 xlist = []
 ylist = []
-x3list = []
-yclist = []
 point_list = []
 added_point_list = []
 added_same_point_list = []
 added_dif_point_list = []
 menge_list = []
 count_list = []
-new_count_list = []
-
-xlist.append(0)
-ylist.append(0)
-point_list.append(Point(0, 0))
 
 # making the list of squares
 for x in range(1, prime):
@@ -73,58 +65,66 @@ plt.grid()
 for v in range(point_list.__len__()):
     e = point_list[v].x
     f = point_list[v].y
-    plt.scatter(e, f, label="stars", color="green",
-               marker="o", s=70)
-print("scatterplot points (green circles): ", point_list, "\n", len(point_list), "points in total \n")
+    plt.scatter(e, f, label="stars", color="green", marker="o", s=70)
+print(colored("scatterplot points (green circles): ", "green"), point_list, "\n", len(point_list), "points in total")
+
+
+def dif_points(x1, x2, y1, y2):
+    nenner = ((x2 - x1) ** (prime - 2)) % prime
+    M = ((y2 - y1) * nenner) % prime
+    M2 = (M * M) % prime
+    x3 = (M2 - x1 - x2) % prime
+    y3 = (M * (x1 - x3) - y1) % prime
+    plt.scatter(x3, y3, label="stars", color="red", marker="*", s=30)
+    added_point_list.append(Point(x3, y3))
+    added_dif_point_list.append(Point(x3, y3))
+    return Point(x3, y3)
+
+
+def same_points(x1, y1):
+    nenner2 = ((2 * y1) ** (prime - 2)) % prime
+    M3 = ((3 * (x1 ** 2) + a) * nenner2) % prime
+    M4 = (M3 * M3) % prime
+    x3 = (M4 - (2 * x1)) % prime
+    y3 = (M3 * (x1 - x3) - y1) % prime
+    plt.scatter(x3, y3, label="stars", color="blue", marker="x", s=60)
+    added_point_list.append(Point(x3, y3))
+    added_same_point_list.append(Point(x3, y3))
+    return Point(x3, y3)
 
 
 def two_points():
-    for i in range(len(xlist)):
-        x1 = xlist[i]
-        y1 = ylist[i]
-        for j in range(i + 1, len(xlist)):
-            # different points
-            x2 = xlist[j]
-            y2 = ylist[j]
+    for i in range(len(point_list)):
+        x1 = point_list[i].x
+        y1 = point_list[i].y
+        for j in range(i + 1, len(point_list)):
+            x2 = point_list[j].x
+            y2 = point_list[j].y
+            if x1 == -1:
+                added_point_list.append(Point(x2, y2))
+            if x2 == -1:
+                added_point_list.append(Point(x1, y1))
+            if x1 == x2 and (y1 + y2) % prime == 0:
+                added_point_list.append(Point(-1, -1))
             if x1 != 0:
                 if x1 != x2:
-                    nenner = ((x2 - x1) ** (prime - 2)) % prime
-                    M = ((y2 - y1) * nenner) % prime
-                    M2 = (M*M) % prime
-                    x3 = (M2 - x1 - x2) % prime
-                    y3 = (M * (x1 - x3) - y1) % prime
-                    # print("aaaaaa", x1, y1, x2, y2, x3, y3)
-                    plt.scatter(x3, y3, label="stars", color="red", marker="*", s=30)
-                    added_point_list.append(Point(x3, y3))
-                    added_dif_point_list.append(Point(x3, y3))
-        # same points
-        nenner2 = ((2 * y1)**(prime - 2)) % prime
-        M3 = ((3 * (x1**2) + a) * nenner2) % prime
-        M4 = (M3*M3) % prime
-        xx3 = (M4 - (2 * x1)) % prime
-        yy3 = (M3 * (x1 - xx3) - y1) % prime
-        # print("bbbbb", x1, y1, nenner2, xx3, yy3)
-
-        plt.scatter(xx3, yy3, label="stars", color="blue", marker="x", s=60)
-        added_point_list.append(Point(xx3, yy3))
-        added_same_point_list.append(Point(xx3, yy3))
-
-    print("ALL added points: \n", ', '.join(str(item) for item in added_point_list), "\n", len(added_point_list),
-          "points in total")
+                    dif_points(x1, x2, y1, y2)
+        same_points(x1, y1)
     print("------------------------------------------------------------------------------------")
-    print("Square added points (blue crosses): \n",
+    print(colored("ALL added points: \n", "yellow"),
+          ', '.join(str(item) for item in added_point_list), "\n", len(added_point_list), "points in total")
+    print("------------------------------------------------------------------------------------")
+    print(colored("Square added points (blue crosses): \n", "blue"),
           ', '.join(str(item) for item in added_same_point_list), "\n", len(added_same_point_list), "points in total")
     print("------------------------------------------------------------------------------------")
-    print("Different added points (red stars): \n",
+    print(colored("Different added points (red stars): \n", "red"),
           ', '.join(str(item) for item in added_dif_point_list), "\n", len(added_dif_point_list), "points in total")
     print("------------------------------------------------------------------------------------")
 
 
-# end of two point calc
 two_points()
 
 
-# punkt als menge (x,y); ((x,y)+(x,y)); ((x,y)+(x,y)+(x,y)) .... bis y-y = 0 (zyklisch)
 def primes(n):
     primfac = []
     d = 2
@@ -138,87 +138,85 @@ def primes(n):
     print("Primzahlzerlegung: ", primfac)
 
 
-def mengen(n):
-    count = 1
-    menge_list.clear()
-    menge_list.append(Point(added_same_point_list[n].x, added_same_point_list[n].y))
-    mathex = (added_same_point_list[n].x + added_same_point_list[n].x) % prime
-    mathey = (added_same_point_list[n].y + added_same_point_list[n].y) % prime
-    menge_list.append(Point(mathex, mathey))
-    for i in range(1, 500):
-        if menge_list[-1].y == menge_list[0].y:
-            break
-        else:
-            mathex = (menge_list[i].x + menge_list[0].x) % prime
-            mathey = (menge_list[i].y + menge_list[0].y) % prime
-            menge_list.append(Point(mathex, mathey))
-            count += 1
-    # remove last item because double
-    menge_list.pop()
-    print("menge", menge_list)
-    print("counter", count)
-    count_list.append(count)
-    # primes(count)   # primfaktorzerlegung; macht nix, wenn count ne prime ist
+def mengen():
+    for i in range(len(point_list)):
+        x1 = point_list[i].x
+        y1 = point_list[i].y
+        x2 = x1
+        y2 = y1
+        k = 1
+        print((x1, y1), end='')
+        x3 = 4711
+        y3 = 4711
+        while True:
+            if x1 == -1:
+                x3 = x2
+                y3 = y2
+            elif x2 == -1:
+                x3 = x1
+                y3 = y1
+            elif x1 == x2 and (y1 + y2) % prime == 0:
+                x3 = -1
+                y3 = -1
+            elif x1 != x2:
+                nenner = ((x2 - x1) ** (prime - 2)) % prime
+                M = ((y2 - y1) * nenner) % prime
+                M2 = (M * M) % prime
+                x3 = (M2 - x1 - x2) % prime
+                y3 = (M * (x1 - x3) - y1) % prime
+            else:
+                nenner = ((2 * y1) ** (prime - 2)) % prime
+                M3 = ((3 * (x1 ** 2) + a) * nenner) % prime
+                M4 = (M3 * M3) % prime
+                x3 = (M4 - (2 * x1)) % prime
+                y3 = (M3 * (x1 - x3) - y1) % prime
+            print((x3, y3), end='')
+            x2 = x3
+            y2 = y3
+            k += 1
+            if x3 == -1:
+                print(" gruppengröße:", k, end='')
+                count_list.append(k)
+                break
+            if k > 75:
+                break
+        print()
 
 
-for i in range(added_same_point_list.__len__()):
-    mengen(i)
+mengen()
 print("countliste: ", count_list)
-
 kgv = np.lcm.reduce(count_list)
 print("kgv von countliste: ", kgv)
 
 
-def hasse():
+def hasse(kgv):
     minimum = prime + 1 - (2 * np.sqrt(prime))
     maximum = prime + 1 + (2 * np.sqrt(prime))
     print("Hasse-Intervall von: ", minimum, " ------------ ", prime, " ------------ ", maximum)
+    if minimum < kgv < maximum:
+        print("KGV ist im Hasse-Intervall")
 
 
-hasse()
-
-for i in range(len(xlist)):
-    x1 = xlist[i]
-    y1 = ylist[i]
-    x2 = x1
-    y2 = y1
-    k = 0
-    print((x1, y1), end='')
-    x3 = 4711
-    y3 = 4711
-    while True:
-        if x1 != x2:
-            nenner = ((x2 - x1) ** (prime - 2)) % prime
-            M = ((y2 - y1) * nenner) % prime
-            M2 = (M * M) % prime
-            x3 = (M2 - x1 - x2) % prime
-            y3 = (M * (x1 - x3) - y1) % prime
-        else:
-            nenner = ((2 * y1) ** (prime - 2)) % prime
-            M3 = ((3 * (x1 ** 2) + a) * nenner) % prime
-            M4 = (M3 * M3) % prime
-            x3 = (M4 - (2 * x1)) % prime
-            y3 = (M3 * (x1 - x3) - y1) % prime
-        print((x3, y3), end='')
-        x2 = x3
-        y2 = y3
-        k += 1
-        if x3 == x1:
-            print((x1, y1), "gruppengröße:", k + 2, end='')
-            break
-        # print(x3 == x1)
-        if k > 75:
-            break
-    print()
+hasse(kgv)
+# plt.show()
 
 
-plt.show()
+def chaos():
+    G = Point(12, 15)
+    n = 3
+    result = Point(-1, -1)
+    while n != 0:
+        if n & 1:
+            result = dif_points(G.x, result.x, G.y, result.y)
+        n = n // 2
+        G = same_points(G.x, G.y)
+    print(G)
 
+
+chaos()
 ''' TODO:
-    - kgv = gruppengröße
-    - gruppengröße im bereich hasse intervall
     - testmenge machen
     - in testmenge reinschieben
-    - verschieben (2l + 1 zur seite springen (schablone verschieben)
+    - verschieben (2l + 1 zur seite springen (schablone verschieben))
     - id bzw #1 finden
 '''
--- ecdsa.py
+++ ecdsa.py
@@ -1,15 +1,14 @@
 import random
 import hashlib
+import matplotlib.pyplot as plt
 from collections import namedtuple
-
-
 Point = namedtuple("Point", "x y")
 
 #######################################################################################################################
 #   Verschlüsseln
 #######################################################################################################################
 
-with open('Worte') as f:
+with open('C:/Users/Julia/Documents/Zimtschnecken.txt') as f:
     a = f.readlines()
 
 # initialize a string
@@ -20,26 +19,46 @@ encoded_str = str1.encode()
 obj_sha3_256 = hashlib.sha3_256(encoded_str)
 M = obj_sha3_256.hexdigest()
 # print in hexadecimal
-print("\nSHA3-256 Hash: ", M)
+print("\nSHA3-256 Hash in hexa: ", M)
 m = int(M, 16)
-print(m)
+print("Hash als int: ", m)
 '''
 M = our hashed message
-dA = A’s private key
-(p,a,b,G,n,h) => domain parameter from elliptic curve
-G = Punkt(12, 15)
+(prime,a,b,G,n,h) => domain parameter from elliptic curve
+G = Punkt(12, 15) Startpunkt von der Gruppe mit Größe n
 '''
-dA = 4711
-G = Point(12, 15)
-n = 15
-a = 1
-b = 5
-prime = 19
+dA = 4711           # private key
+prime = 19          # prime
+a = 1               # a from curve
+b = 5               # b from curve
+G = Point(12, 15)   # basepoint from group size n
+n = 15              # size of group
+# h = ?
 r = G.x % n
 
 
+def dif_points(x1, x2, y1, y2):
+    nenner = ((x2 - x1) ** (prime - 2)) % prime
+    M = ((y2 - y1) * nenner) % prime
+    M2 = (M * M) % prime
+    x3 = (M2 - x1 - x2) % prime
+    y3 = (M * (x1 - x3) - y1) % prime
+    plt.scatter(x3, y3, label="stars", color="red", marker="*", s=30)
+    return Point(x3, y3)
+
+
+def same_points(x1, y1):
+    nenner2 = ((2 * y1) ** (prime - 2)) % prime
+    M3 = ((3 * (x1 ** 2) + a) * nenner2) % prime
+    M4 = (M3 * M3) % prime
+    x3 = (M4 - (2 * x1)) % prime
+    y3 = (M3 * (x1 - x3) - y1) % prime
+    plt.scatter(x3, y3, label="stars", color="blue", marker="x", s=60)
+    return Point(x3, y3)
+
+
 def dsa():
-    k = random.randint(1, n - 1)   # bis 14 weil gruppengröße 15 bei uns?
+    k = random.randint(1, n - 1)   # bis 14 weil gruppengröße 15 bei uns
     kinv = (k**(n - 2)) % n
     s = (kinv * (r * dA + m)) % n
     if s != 0:
@@ -57,17 +76,39 @@ print(mes)
 #######################################################################################################################
 #   Verifikation
 #######################################################################################################################
-
+pA = 1111           # public key
+print("\nVerifikation")
 mes_list = mes.split(",")
 print(mes_list)
+r1 = int(mes_list[1])
+s1 = int(mes_list[2])
+mes1 = int(mes_list[0])
 
-if 1 < int(mes_list[1]) and int(mes_list[2]) < (n - 1):
-    print("yay")
+if 1 < (r1 and s1) < (n - 1):
     sinv = (int(mes_list[2]) ** (n - 2)) % n
-    u1 = (sinv * int(mes_list[0])) % n
-    u2 = (sinv * int(mes_list[1])) % n
+    u1 = (sinv * mes1) % n
+    u2 = (sinv * r1) % n
     print("u1", u1, "u2", u2)
-
-
 else:
     print("Werte nicht im Rahmen")
+
+
+def chaos(G, n):
+    result = Point(-1, -1)
+    while n != 0:
+        if n & 1:
+            result = dif_points(G.x, result.x, G.y, result.y)
+        n = n // 2
+        G = same_points(G.x, G.y)
+    print(G)
+    return G
+
+
+Q = chaos(G, u1)            # + (u2 * pA) wie addier ich das das sind 2 zahlen help
+if Q == 0:
+    print("Q ist null, also falsch")
+v = Q.x % n
+if v == r:
+    print("Alles richtig, v =", v, "r =", r )
+else:
+    print("Nicht richtig, v =", v, "r =", r)
